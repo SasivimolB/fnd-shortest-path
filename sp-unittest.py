45c93c5f85cd442aa2d9b2d8b6862dbff429ab0d
SasivimolB <pangsasi.mol@gmail.com>
2022-01-25 23:42:58 +0700

    unittest add

--- sp-unittest.py
+++ sp-unittest.py
@@ -1,27 +1,56 @@
-from spa import createSPtable, findSP, getPath, getSP
+from this import d
+from spa import ShortestPath
 import unittest
 class TestSP(unittest.TestCase):
+    spa = ShortestPath()
     def setUp(self):
-        self.graphInfo = [
-            ['A', 'B', '5'], 
-            ['A', 'D', '3'], 
-            ['A', 'E', '4'], 
-            ['B', 'C', '4'], 
-            ['E', 'F', '6'], 
-            ['C', 'G', '2'], 
-            ['D', 'G', '6'], 
-            ['G', 'H', '3'], 
-            ['H', 'F', '5'], 
-            ['I', '0', '0']]
-        self.vertices = createSPtable(self.graphInfo)
+        self.graphInfo = self.spa.getinfo("graph.csv")
+        self.vertices = self.spa.createSPtable(self.graphInfo)
+    
+    def test_cost1(self):
+        start = 'A'
+        end = 'B'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.sp, 5)
+    def test_path1(self):
+        start = 'A'
+        end = 'B'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.spa.getPath(self.vertices, start, end), 'A->B')
 
-    def test_num_vertices(self):
-        self.assertEqual(len(self.vertices), 8)
+    def test_cost2(self):
+        start = 'C'
+        end = 'I'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.sp, 0)
+    def test_path2(self):
+        start = 'C'
+        end = 'I'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.spa.getPath(self.vertices, start, end), 'No path')
 
-    def test_path_to_goal(self):
-        findSP(self.graphInfo, self.vertices, 'A')
-        self.assertEqual(getPath(self.vertices, 'A', 'I'), 'No path')
-        self.assertEqual(getSP(self.vertices, 'C'), 10)
+    def test_cost3(self):
+        start = 'H'
+        end = 'H'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.sp, 0)
+    def test_path3(self):
+        start = 'H'
+        end = 'H'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.spa.getPath(self.vertices, start, end), 'H->H')
+
+    def test_cost4(self):
+        start = 'P'
+        end = 'A'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.sp, 0)
+    def test_path4(self):
+        start = 'P'
+        end = 'A'
+        self.sp = self.spa.findSP(self.graphInfo, self.vertices, start, end)
+        self.assertEqual(self.spa.getPath(self.vertices, start, end), 'No path')
+
         
 if __name__ == '__main__':
     unittest.main()
--- spa.py
+++ spa.py
@@ -1,91 +1,80 @@
 import csv
 import heapq
 
-def getinfo(fileName):
-    file = open(fileName)
-    csvreader = csv.reader(file)
-    info = []
-    for row in csvreader:
-        info.append(row)
-    file.close()
-    return info
+class ShortestPath:
 
-def createSPtable(graphInfo):
-    vertices =  {}
-    for i in range(len(graphInfo)):
-        for j in range(2):
-            if(graphInfo[i][j] != '0'):
-                vertices[graphInfo[i][j]] = [float('inf'), '-']
-            else:
-                vertices[graphInfo[i][0]] = [0, '-']
-    # [print(key,':',value) for key, value in vertices.items()]
-    return vertices
+    def getinfo(self, fileName):
+        file = open(fileName)
+        csvreader = csv.reader(file)
+        info = []
+        for row in csvreader:
+            info.append(row)
+        file.close()
+        return info
 
-def findSP(graphInfo, vertices, start):
-    vertices[start] = [0, '-']
-    visited = set()
-    pq = []
-    heapq.heappush(pq, (0, start))
-    while pq:
-        oldcost, node = heapq.heappop(pq)
-        visited.add(node)
+    def createSPtable(self, graphInfo):
+        vertices =  {}
         for i in range(len(graphInfo)):
-            adjacentnode = ''
-            if(graphInfo[i][0] == node): adjacentnode = graphInfo[i][1]
-            elif(graphInfo[i][1] == node): adjacentnode = graphInfo[i][0]
-
-            if(adjacentnode and (not(adjacentnode == '0'))):
-                if(adjacentnode in visited): continue
+            for j in range(2):
+                if(graphInfo[i][j] != '0'):
+                    vertices[graphInfo[i][j]] = [float('inf'), '-']
                 else:
-                    newcost = (int)(graphInfo[i][2]) + oldcost
-                    if(vertices[adjacentnode][0] > newcost):
-                        vertices[adjacentnode] = [newcost, node]
-                        heapq.heappush(pq, (newcost, adjacentnode))
+                    vertices[graphInfo[i][0]] = [0, '-']
         # [print(key,':',value) for key, value in vertices.items()]
-    return vertices
+        return vertices
 
-def getPath(vertices, start, end):
-    prevnode = end
-    revpath = [] 
-    revpath.append(end)
-    while not(prevnode == start):
-        if not(vertices[prevnode][1] == '-'):
-            prevnode = vertices[prevnode][1]
-            revpath.append(prevnode)
-        else: revpath = []
+    def findSP(self, graphInfo, vertices, start, end):
+        vertices[start] = [0, '-']
+        visited = set()
+        pq = []
+        heapq.heappush(pq, (0, start))
+        while pq:
+            oldcost, node = heapq.heappop(pq)
+            visited.add(node)
+            for i in range(len(graphInfo)):
+                adjacentnode = ''
+                if(graphInfo[i][0] == node): adjacentnode = graphInfo[i][1]
+                elif(graphInfo[i][1] == node): adjacentnode = graphInfo[i][0]
 
-    if(revpath):
-        path = start
-        for i in range(len(revpath)):
-            node = revpath.pop()
-            if(not(node == start)): path = path + "->" + node
-        return path
-    else:
-        return("No path")
+                if(adjacentnode and (not(adjacentnode == '0'))):
+                    if(adjacentnode in visited): continue
+                    else:
+                        newcost = (int)(graphInfo[i][2]) + oldcost
+                        if(vertices[adjacentnode][0] > newcost):
+                            vertices[adjacentnode] = [newcost, node]
+                            heapq.heappush(pq, (newcost, adjacentnode))
+            # [print(key,':',value) for key, value in vertices.items()]
+        return vertices[end][0]
 
-def getSP(vertices, end):
-    return vertices[end][0]
+    def getPath(vertices, start, end):
+        prevnode = end
+        revpath = [] 
+        revpath.append(end)
+        while not(prevnode == start):
+            if not(vertices[prevnode][1] == '-'):
+                prevnode = vertices[prevnode][1]
+                revpath.append(prevnode)
+            else: revpath = []
 
-    
-# fileName = input("What is graph file name: ")
-# graphInfo = getinfo(fileName)
-graphInfo = getinfo("graph.csv")
-start = input("What is start node: ").upper()
-end = input("What is goal node: ").upper()
+        if(revpath):
+            path = start
+            for i in range(len(revpath)):
+                node = revpath.pop()
+                if(not(node == start)): path = path + "->" + node
+            return path
+        else:
+            return("No path")
 
-vertices = createSPtable(graphInfo)
-if (not(start in vertices) or not(end in vertices)):
-    print("No path")
-elif ((start == end) and ((start in vertices) or (end in vertices))): 
-    print("Path from " + start + " to " + end + " is " + start +"->" + end + ", and have cost 0.")
-elif ((vertices[start] == [0, '-']) or (vertices[end] == [0, '-'])):
-    print("No path")
-else:
-    shortestpath = findSP(graphInfo, vertices, start)
-    path = getPath(vertices, start, end)
+# spa = ShortestPath()
 
-    if(not(path == 'No path')):
-        sp = vertices[end][0]
-        print("Path from " + start + " to " + end + " is " + path + ", and have cost " + str(sp) + ".")
-    else:
-        print("No path")
+# graphInfo = spa.getinfo("graph.csv")
+# start = input("What is start node: ").upper()
+# end = input("What is goal node: ").upper()
+
+# vertices = spa.createSPtable(graphInfo)
+# sp = spa.findSP(graphInfo, vertices, start, end)
+# path = spa.getPath(vertices, start, end)
+ 
+# if(not(path == 'No path')):
+#     print("Path from " + start + " to " + end + " is " + path + ", and have cost " + str(sp) + ".")
+# else: print("No path")
